e1a9b2a1106eee7b3e27d3f674ce8df0d5fba85a
patience <patiencewanjiru123@gmail.com>
2023-03-14 00:02:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Patience Wanjiru';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting =  `Welcome, ${currentUser.slice(0,1)}!`;
